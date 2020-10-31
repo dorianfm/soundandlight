57e80ad5119452afa45d07fe6a8bf4cb10ba368f
Dorian Fraser-Moore <dorian+code@theusefularts.org>
2020-10-31 14:49:18 +0000

    Arduino code to check which led is closest

---
+++ arduino/NearFar/NearFar.ino
@@ -0,0 +1,35 @@
+  #include <FastLED.h>
+  #define LED_PIN     3
+  #define LED2_PIN    9
+  #define NUM_LEDS    149
+
+  CRGB leds[NUM_LEDS];
+  CRGB leds2[NUM_LEDS];
+
+  int g;
+  int up;
+
+  void setup() {
+      FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+      FastLED.addLeds<WS2812, LED2_PIN, GRB>(leds2, NUM_LEDS);
+      g=0;
+      up=1;
+  }
+
+  void loop() {
+      leds[1] = CRGB(g, 0, 0);
+      leds2[1] = CRGB(0, 0, g);
+      FastLED.show();
+      if (up) {
+        g++;
+      } else {
+        g--;
+      }
+      if (g <= 1)
+      {
+        up = 1;
+      } else if (g >= 149) {
+        up = 0;
+      }
+      delay(30);
+  }
